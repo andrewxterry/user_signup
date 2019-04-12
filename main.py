4586ca84b806eaa98d84e9c97bf1597436e35d72
Andrew Terry <andrewxtery@googlemail.com>
2019-04-12 13:53:23 -0500

    Initial website design

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+app.run()
